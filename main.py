cf6423aa0a79c032db75f659e899983833e2195f
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2026-06-04 12:20:39 +0500

    feat: add player movement functionality and optimize player initialization

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
 from player import Player
+
+
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -9,18 +11,17 @@ def main():
     dt = 0.0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    
+    player = Player(x, y)
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player = Player(x, y)
         player.update(dt)
         player.draw(screen)
         pygame.display.flip()
-        dt = clock.tick(60) / 1000 # save in seconds
+        dt = clock.tick(60) / 1000  # save in seconds
 
 
 if __name__ == "__main__":
