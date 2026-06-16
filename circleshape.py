cae61a2408a0cae864ab47016e14cd9471cf5bc2
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2026-06-16 14:00:29 +0500

    added collision detection

--- circleshape.py
+++ circleshape.py
@@ -1,18 +1,19 @@
 import pygame
 
-class CircleShape(pygame.sprite.Sprite):
-    containers: tuple[pygame.sprite.Group, ...] # what are ... here ?
 
-    def __init__(self, x: float, y:float, radius: float) -> None:
+class CircleShape(pygame.sprite.Sprite):
+    containers: tuple[pygame.sprite.Group, ...]  # what are ... here ?
+
+    def __init__(self, x: float, y: float, radius: float) -> None:
         if hasattr(self, "containers"):
             super().__init__(*self.containers)
-        else: 
+        else:
             super().__init__()
-        
+
         self.position: pygame.Vector2 = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0,0)
+        self.velocity = pygame.Vector2(0, 0)
         self.radius = radius
-    
+
     def draw(self, screen: pygame.Surface) -> None:
         # must override
         pass
@@ -20,3 +21,11 @@ class CircleShape(pygame.sprite.Sprite):
     def update(self, dt: float) -> None:
         # must override
         pass
+
+    def collides_with(self, other):
+        distance = self.position.distance_to(other.position)
+        radiusSum = self.radius + other.radius
+        if distance >= radiusSum:
+            return False
+        return True
+
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
-from logger import log_state
+from logger import log_event, log_state
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 
 def main():
@@ -28,6 +29,11 @@ def main():
                 return
         screen.fill("black")
         updatable.update(dt)
+        for thing in asteroids:
+            if thing.collides_with(player) == True:
+                log_event("player_hit")
+                print("Game Over!")
+                sys.exit()
         for thing in drawable:
             thing.draw(screen)
         pygame.display.flip()
